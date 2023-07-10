2128aa5ee0586d687d94648b57c33bfc503f3c13
Anurag-Yadav03 <03yadavanurag@gmail.com>
2023-07-10 17:58:45 +0530

    completed vercel

--- server/app.js
+++ server/app.js
@@ -11,8 +11,6 @@ const authRouter = require('./routes/authRoutes');
 const userRouter = require('./routes/userRoutes');
 const adminRouter = require('./routes/adminRoutes');
 
-
-
 app.use('/api/v1', authRouter)
 app.use('/api/v1/user',userRouter);
 app.use('/api/v1/admin',adminRouter);
