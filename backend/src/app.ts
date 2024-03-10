146af6f130a2ad0ac8d45f2acbf3b9a3a6ae102f
harryadav3 <03yadavanurag@gmail.com>
2024-03-11 03:10:13 +0530

    deployedment 1

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,6 +14,22 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+
+app.use("/", (req: Request, res: Response, next: NextFunction) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+}
+);
+
+
 app.use('/api/auth/', authRoutes);
 app.use('/api/instructors', instructorRoutes);
 app.use('/api/courses', courseRoutes);
