a18ab4a42af00fef4e17073ef073c28316d5375c
harryadav3 <03yadavanurag@gmail.com>
2024-03-11 07:37:16 +0530

    added isntructor and student

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,6 +1,9 @@
 import { useAuthStore } from "../store/store";
 import companiesImage from "./../assets/companies.svg"; // replace with actual path
 import { Link } from 'react-router-dom'
+
+import instructorImage from "./../assets/instructor.svg"; // replace with actual path
+import studentImage from "./../assets/student.svg"; // replace with actual path
 const Hero = () => {
 
 const {isLoggedIn} = useAuthStore((state) => state);
@@ -8,8 +11,8 @@ const {isLoggedIn} = useAuthStore((state) => state);
 return (
     <>
         <div className="bg-white py-16">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 flex-col items-center justify-center lg:px-40">
-                <h1 className=" text-4xl lg:text-6xl font-bold  text-center mb-8">
+            <div className=" relative max-w-7xl mx-auto px-4 sm:px-6 flex-col items-center justify-center lg:px-40 ">
+                <h1 className=" text-4xl lg:text-6xl font-bold mb-14 text-center lg:mb-8">
                     Teaching in the Internet age means we must teach{" "}
                     <span className="text-purple-600">tomorrow's</span> skills today
                 </h1>
@@ -17,8 +20,16 @@ return (
                     Provides you with the latest online learning system and material
                     that help your
                 </p>
-
                 
+                <div className="flex  justify-around py-2 lg:gap-96 absolute w-full  left-0 top-44 lg:top-40">
+                    
+                 <img src={instructorImage} alt="" className="w-12  lg:w-28  mx-auto" />
+               
+               
+                <img src={studentImage} alt="" className="lg:w-28 w-12 mx-auto" />
+              
+                </div>
+
 
                 { isLoggedIn ? (
                     <div className="flex justify-center space-x-4">
